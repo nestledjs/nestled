53a063c1b7310f0951c786f52f6af42a2c33cc4e
Justin Handley <justin@justinhandley.com>
2025-06-13 14:17:40 -0400

    Fix libs path in auth plugin

--- generators/plugins/src/auth/generator.ts
+++ generators/plugins/src/auth/generator.ts
@@ -3,7 +3,7 @@ import { getNpmScope } from '@nx/js/src/utils/package-json/get-npm-scope'
 import { addToModules } from '@nestled/utils'
 
 export default async function generator(tree: Tree) {
-  const directory = 'api/custom/plugins'
+  const directory = 'api/custom/src/lib/plugins'
   const overwrite = false
   const npmScope = getNpmScope(tree)
   const destRoot = joinPathFragments('libs', directory, 'auth')
@@ -25,7 +25,7 @@ export default async function generator(tree: Tree) {
 
   // Add export to /libs/api/custom/src/index.ts
   const indexPath = 'libs/api/custom/src/index.ts'
-  const exportStatement = `export * from './libs/plugins/auth/auth.module'\n`
+  const exportStatement = `export * from './lib/plugins/auth/auth.module'\n`
   if (tree.exists(indexPath)) {
     const content = tree.read(indexPath, 'utf-8')
     if (!content.includes(exportStatement.trim())) {
