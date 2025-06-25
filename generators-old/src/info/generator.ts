532c1fb89881c26e1c249975dd7adcf79ea4e1e2
Justin Handley <justin@justinhandley.com>
2025-06-24 20:41:12 -0400

    Update to nx 21.2.1

--- generators-old/src/info/generator.ts
+++ generators-old/src/info/generator.ts
@@ -10,7 +10,7 @@ Run the following commands in order to set up your project:
 nx g @nestledjs/generators:api-dependencies
 \`\`\`
 > Note: This will automatically run \`pnpm install\` to install all dependencies.
-> It will also set up Nx packages to version 21.2.0 for compatibility.
+> It will also set up Nx packages to version 21.2.1 for compatibility.
 
 \`\`\`sh
 nx g @nestledjs/generators:api-app
--- generators/web/src/setup/generator.ts
+++ generators/web/src/setup/generator.ts
@@ -18,7 +18,7 @@ export async function webSetupGenerator(tree: Tree): Promise<GeneratorCallback> 
       '@apollo/client-integration-react-router': '0.12.0-alpha.4',
     },
     {
-      '@nx/react': '21.2.0',
+      '@nx/react': '21.2.1',
       'vite-tsconfig-paths': '^5.1.4',
       '@tailwindcss/vite': '^4.1.8',
       tailwindcss: '^4.1.8',
