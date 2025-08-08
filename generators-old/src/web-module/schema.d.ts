3b26712f292a4859b091b826803e256140ce4c18
Justin Handley <justin@justinhandley.com>
2025-08-08 17:50:07 -0400

    Remove all previous generators

--- generators-old/src/web-module/schema.d.ts
+++
@@ -1,3 +0,0 @@
-export interface WebModuleGeneratorSchema {
-  name: string
-}
--- generators-old/src/web-user-crud/schema.d.ts
+++
@@ -1,5 +0,0 @@
-export interface WebUserCrudGeneratorSchema {
-  name: string
-  plural: string
-  primaryField: string
-}
