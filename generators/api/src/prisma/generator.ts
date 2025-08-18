f6521b296826ccdae596e2f20d839464bac805b7
Justin Handley <justin@justinhandley.com>
2025-08-18 12:46:54 -0400

    refactor(api): use optional chaining when removing deprecated prisma.schema from package.json

--- generators/api/src/prisma/generator.ts
+++ generators/api/src/prisma/generator.ts
@@ -14,7 +14,7 @@ export default async function generateLibraries(tree: Tree, options: ApiPrismaGe
 
   // Update package.json scripts (remove deprecated prisma.schema field if present)
   updateJson(tree, 'package.json', (json) => {
-    if (json.prisma && json.prisma.schema) {
+    if (json.prisma?.schema) {
       delete json.prisma.schema
     }
     if (!json.prisma) {
